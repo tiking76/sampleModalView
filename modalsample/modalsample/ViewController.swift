3015ed4b90c9ab2cad3a595b3eea3b7b11075025
tiking76 <716yosiki1999@gmail.com>
2020-10-27 00:53:38 +0900

    :tada:initial commit

---
+++ modalsample/modalsample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  modalsample
+//
+//  Created by 舘佳紀 on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
